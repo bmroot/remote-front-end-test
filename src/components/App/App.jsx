80577b02280c00f7ee77657957c7a2ae88d92076
Bakhtilar Muzakparov <bakhtilar.muzakparov@UKHOITMB3824.local>
2024-06-13 09:03:50 +0100

    use api for properties

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import Header from '../Header';
 import SortAndFilter from '../SortAndFilter';
 import PropertyListing from '../PropertyListing';
 
 const App = () => {
+    const [properties, setProperties] = useState([]);
+
+    useEffect(() => {
+        async function getProperties() {
+            const res = await fetch('http://localhost:3000/api/properties');
+            const data = await res.json();
+            return data;
+        }
+
+        getProperties().then((data) => setProperties(data));
+    }, []);
+
     return (
         <div className="App">
             <Header />
             <main>
                 <SortAndFilter />
-                <PropertyListing />
+                <PropertyListing properties={properties} />
             </main>
         </div>
     );
--- src/components/PropertyListing/tests/PropertyListing.spec.jsx
+++ src/components/PropertyListing/tests/PropertyListing.spec.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { within } from '@testing-library/dom';
 import PropertyListing from '../PropertyListing';
+import { DUMMY_PROPERTY } from '../../../mocks';
 
 describe('PropertyListing', () => {
     it('should render five property cards', async () => {
-        render(<PropertyListing />);
+        render(<PropertyListing properties={Array(5).fill(DUMMY_PROPERTY)} />);
         const propertiesList = screen.getByRole('list');
         const propertyCards = await within(propertiesList).findAllByRole('listitem');
         expect(propertyCards).toHaveLength(5);
